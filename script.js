b923b7148d07517245d8d88bf98b91f6de7672e1
SUDEV MAJHI <58890958+Sudev0707@users.noreply.github.com>
2026-01-02 02:17:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 //your JS code here. If required.
+
+function daysOfYear(year){
+	if (year !>== 1 && !<= 9999){
+		return console.log('enter valid year')
+	}
+
+	if(year %400 == 0 || (year %4 == 0 && year %100 !== 0)){
+		return 366
+	}else{
+		return 365
+	}
+}
+
+daysOfYear(2024)
